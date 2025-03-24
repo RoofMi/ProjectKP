8420ef4f5e3bd18fbdf641a99c5d1edd589c5b1f
ZeNiff <leejbsocial@gmail.com>
2025-03-24 19:00:32 +0900

    feat : Time의 동작 방식 수정 및 아이콘 추가
    - Unity의 coroutine은 유동적인 상황에서 사용하기 적합
    - 전체 시간을 확인하는 Time으로선 성능적으로 Update가 유용하기 때문에 이를 수정함
    - Score를 직관적으로 보여주기 위해 아이콘을 추가
    - 튜토리얼 파일을 삭제

--- Assets/Scripts/System/VersusModeManager.cs
+++ Assets/Scripts/System/VersusModeManager.cs
@@ -5,18 +5,21 @@ using UnityEngine.UIElements;
 
 public class VersusModeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private TextMeshProUGUI PlayerScoreText;
     private TextMeshProUGUI EnemyScoreText;
     private TextMeshProUGUI TimeText;
 
+    /**
+     * 정보 저장 변수들
+     * @param ***Score 스코어 점수
+     * @param ***Time 대기 시간 및 게임 시간
+     * @param bWait, Min, Sec 시간 관리에 필요한 변수
+     */
     private float PlayerScore = 0;
     private float EnemyScore = 0;
-
     private float PreparationTime = 5.0f;
     private float RemainTime = 80.0f;
     private bool bWait = false;
-
     private int Min;
     private int Sec;
 
@@ -34,10 +37,21 @@ public class VersusModeManager : MonoBehaviour
         PlayerScoreText.text = "" + (int)PlayerScore;
         EnemyScoreText.text = "" + (int)EnemyScore;
         TimeText.text = "" + (int)PreparationTime;
+
+        // Time.deltaTime에 의해 1초가 빠르게 지나가므로 원하는 시간 설정을 위한 1초 추가
+        PreparationTime += 1.0f;
+        RemainTime += 1.0f;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        TimeSystem();
+
+        //ScoreSystem();
+    }
+    
+    private void TimeSystem()
     {
         // 준비시간 이후에 메인 시간이 흐르도록 설정
         if ((int)PreparationTime == 0)
@@ -56,26 +70,18 @@ public class VersusModeManager : MonoBehaviour
             }
             else
             {
+                //RemainTime++;
+                RemainTime -= Time.deltaTime;
                 TimeTextUpdate(RemainTime);
                 TimeText.color = Color.black;
-
-                Debug.Log("WAIT 1 SECOND");
-                StartCoroutine(Wait());
             }
-            
+
         }
         else
         {
             PreparationTime -= Time.deltaTime;
-
             TimeTextUpdate(PreparationTime);
         }
-    }
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(1.0f);
-
-        bWait = true;
     }
 
     private void TimeTextUpdate(float TimeValue)
@@ -84,5 +90,10 @@ public class VersusModeManager : MonoBehaviour
         Sec = (int)TimeValue % 60;
 
         TimeText.text = "" + string.Format("{0:D2}:{1:D2}", Min, Sec);
+    }
+
+    private void ScoreSystem()
+    {
+
     }
 }
