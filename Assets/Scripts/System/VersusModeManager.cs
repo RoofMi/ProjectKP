5ac04828b3c6526c24337d66b2c6ac04f3734997
ZeNiff <leejbsocial@gmail.com>
2025-03-24 20:59:14 +0900

    feat : Score 시스템 수정 및 게임 종료 구현
    - Score를 Array로 관리하도록 수정
    - Score를 비교하여 승패를 확인하는 기능 추가

--- Assets/Scripts/System/VersusModeManager.cs
+++ Assets/Scripts/System/VersusModeManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
+using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class VersusModeManager : MonoBehaviour
 {
@@ -11,15 +10,13 @@ public class VersusModeManager : MonoBehaviour
 
     /**
      * 정보 저장 변수들
-     * @param ***Score 스코어 점수
+     * @param Score 스코어 점수. 0번이 Player, 1번이 Enemy
      * @param ***Time 대기 시간 및 게임 시간
-     * @param bWait, Min, Sec 시간 관리에 필요한 변수
+     * @param Min, Sec 시간 관리에 필요한 변수
      */
-    private float PlayerScore = 0;
-    private float EnemyScore = 0;
+    private float[] Score = new float[2];
     private float PreparationTime = 5.0f;
-    private float RemainTime = 80.0f;
-    private bool bWait = false;
+    public float RemainTime = 5.0f;
     private int Min;
     private int Sec;
 
@@ -34,21 +31,23 @@ public class VersusModeManager : MonoBehaviour
         if (EnemyScoreText == null)
             Debug.LogWarning("EnemyScoreText is not assigned!");
 
-        PlayerScoreText.text = "" + (int)PlayerScore;
-        EnemyScoreText.text = "" + (int)EnemyScore;
+        PlayerScoreText.text = "" + (int)Score[0];
+        EnemyScoreText.text = "" + (int)Score[1];
         TimeText.text = "" + (int)PreparationTime;
 
         // Time.deltaTime에 의해 1초가 빠르게 지나가므로 원하는 시간 설정을 위한 1초 추가
         PreparationTime += 1.0f;
         RemainTime += 1.0f;
+
+        // 캐릭터의 상태에 따라 점수값을 추가하는 단계 (Enemy는 아직 구현되어 있지 않음)
+        //PlayerState ps = FindFirstObjectByType<PlayerState>();
+        //ps.PlayerDie += new EventHandler(IncreasePlayerScore);
     }
 
     // Update is called once per frame
     void Update()
     {
         TimeSystem();
-
-        //ScoreSystem();
     }
     
     private void TimeSystem()
@@ -56,26 +55,17 @@ public class VersusModeManager : MonoBehaviour
         // 준비시간 이후에 메인 시간이 흐르도록 설정
         if ((int)PreparationTime == 0)
         {
-            if (bWait)
+            if ((int)RemainTime == 0)
             {
-                if ((int)RemainTime == 0)
-                {
-                    Debug.Log("GAME END");
-                }
-                else
-                {
-                    RemainTime -= Time.deltaTime;
-                    TimeTextUpdate(RemainTime);
-                }
+                GameEnd();
             }
             else
             {
-                //RemainTime++;
                 RemainTime -= Time.deltaTime;
-                TimeTextUpdate(RemainTime);
                 TimeText.color = Color.black;
-            }
 
+                TimeTextUpdate(RemainTime);
+            }
         }
         else
         {
@@ -92,8 +82,38 @@ public class VersusModeManager : MonoBehaviour
         TimeText.text = "" + string.Format("{0:D2}:{1:D2}", Min, Sec);
     }
 
-    private void ScoreSystem()
+    private void ScoreTextUpdate(byte idx)
     {
+        switch(idx)
+        {
+            case 0:
+                PlayerScoreText.text = "" + (int)Score[0];
+                break;
+            case 1:
+                EnemyScoreText.text = "" + (int)Score[1];
+                break;
+        }
+    }
 
+    private void IncreasePlayerScore(object sender, EventArgs eventArgs)
+    {
+        Score[0] += 1.0f;
+        ScoreTextUpdate(0);
+    }
+
+    private void IncreaseEnemyScore(object sender, EventArgs eventArgs)
+    {
+        Score[1] += 1.0f;
+        ScoreTextUpdate(1);
+    }
+
+    private void GameEnd()
+    {
+        if (Score[0] > Score[1])
+            Debug.Log("Player Win");
+        else if (Score[0] < Score[1])
+            Debug.Log("Enemy Win");
+        else
+            Debug.Log("DRAW");
     }
 }
